7fe2e9238fbe20d6228d72a428e7ad2e47393b98
viaSSH <hanseung0609@gmail.com>
2022-05-17 15:10:59 +0900

    value change

--- var.tf
+++ var.tf
@@ -1,11 +1,5 @@
 variable "ec2_ids" {
   type        = string
   description = "Your EC2 ID. ex)i-00947d5781febb052, i-07eea10d9ab11f537"
-    # description = "Your EC2 ID. ex)i-087f706171882d2db, i-00947d5781febb055"
-
-# i-087f706171882d2db, i-00947d5781febb055
-#   validation {
-#     condition     = var.iam_user_count < 5
-#     error_message = "IAM user count must be less than 5."
-#   }
+    
 }
